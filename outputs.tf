4762cf26b996dac9ebc4f2955367c82b5149b500
Geoff Garbers <geoff@garbers.co.za>
2019-05-22 06:56:11 +0200

    Ensure that outputs refer to managed addresses only.

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,9 @@
 output address_links {
-  value = ["${local.address_links}"]
+  value = ["${google_compute_address.nat_address.*.self_link}"]
+  description = "Self links of addresses managed by this module."
 }
 
 output addresses {
-  value = ["${local.addresses}"]
+  value = ["${google_compute_address.nat_address.*.address}"]
+  description = "IPs of addresses managed by this module."
 }
