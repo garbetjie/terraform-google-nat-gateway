bfc70edd3606f84241dedf33ea818121c26526b8
Geoff Garbers <geoff@garbers.co.za>
2019-06-03 08:47:06 +0200

    Upgrade to be 0.12 compatible.

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,10 @@
-output address_links {
-  value = ["${google_compute_address.nat_address.*.self_link}"]
+output "address_links" {
+  value       = [google_compute_address.nat_address.*.self_link]
   description = "Self links of addresses managed by this module."
 }
 
-output addresses {
-  value = ["${google_compute_address.nat_address.*.address}"]
+output "addresses" {
+  value       = [google_compute_address.nat_address.*.address]
   description = "IPs of addresses managed by this module."
 }
+
