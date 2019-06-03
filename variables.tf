a0a1a5b646d11ee1f9c5d5be30f58d79b191c45b
Geoff Garbers <geoff@garbers.co.za>
2019-06-03 08:47:06 +0200

    Upgrade to be 0.12 compatible.

--- variables.tf
+++ variables.tf
@@ -1,69 +1,72 @@
-variable name {
-  type = "string"
-  default = ""
-  description = "The name of the Cloud NAT gateway. Defaults to `nat-$${var.region}`."
+variable "name" {
+  type        = string
+  default     = ""
+  description = "The name of the Cloud NAT gateway. Defaults to `nat-$$${var.region}`."
 }
 
-variable region {
-  type = "string"
+variable "region" {
+  type        = string
   description = "The region for which this Cloud NAT gateway is for"
 }
 
-variable address_links {
-  type = "list"
-  default = []
+variable "address_links" {
+  type        = list(string)
+  default     = []
   description = "The self link of addresses managed outside this module to use for the NAT gateway."
 }
 
-variable reserve_addresses {
-  type = "string"
-  default = true
+variable "reserve_addresses" {
+  type        = string
+  default     = true
   description = "Boolean value to indicate whether this module should reserve addresses for use with the NAT gateway."
 }
 
-variable reserve_address_count {
-  type = "string"
-  default = 1
+variable "reserve_address_count" {
+  type        = string
+  default     = 1
   description = "When reserving addresses, the number of addresses to reserve."
 }
 
-variable network {
-  type = "string"
-  default = "default"
+variable "network" {
+  type        = string
+  default     = "default"
   description = "The network to which this Cloud NAT gateway applies."
 }
 
-variable min_ports_per_vm {
-  type = "string"
-  default = 128
+variable "min_ports_per_vm" {
+  type        = string
+  default     = 128
   description = "Minimum number of ports allocated to a VM."
 }
 
-variable udp_idle_timeout_sec {
-  type = "string"
-  default = 30
+variable "udp_idle_timeout_sec" {
+  type        = string
+  default     = 30
   description = "Timeout (in seconds) for UDP connections."
 }
 
-variable tcp_established_idle_timeout_sec {
-  type = "string"
-  default = 1200
+variable "tcp_established_idle_timeout_sec" {
+  type        = string
+  default     = 1200
   description = "Timeout (in seconds) for established TCP connections."
 }
 
-variable tcp_transitory_idle_timeout_sec {
-  type = "string"
-  default = 30
+variable "tcp_transitory_idle_timeout_sec" {
+  type        = string
+  default     = 30
   description = "Timeout (in seconds) for transitory TCP connections."
 }
 
-variable icmp_idle_timeout_sec {
-  type = "string"
-  default = 30
+variable "icmp_idle_timeout_sec" {
+  type        = string
+  default     = 30
   description = "Timeout (in seconds) for ICMP connections."
 }
 
-
 locals {
-  address_links = ["${concat(var.address_links, google_compute_address.nat_address.*.self_link)}"]
+  address_links = [concat(
+    var.address_links,
+    google_compute_address.nat_address.*.self_link,
+  )]
 }
+
