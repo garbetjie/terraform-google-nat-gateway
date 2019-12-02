27e9affc7a00b7d48de68a70ab18e34034c1f24b
Geoff Garbers <geoff@tencent.co.za>
2019-12-02 11:51:34 +0200

    Change google_compute_router name for backwards compatibility.

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "google_compute_router" "router" {
+resource "google_compute_router" "nat_router" {
   name        = var.name == "" ? "nat-${var.region}" : var.name
   network     = var.network
   description = "Cloud router used for the NAT gateway in the ${var.region} region."
@@ -7,7 +7,7 @@ resource "google_compute_router" "router" {
 
 resource "google_compute_router_nat" "nat" {
   name                               = "nat-${var.region}"
-  router                             = google_compute_router.router.name
+  router                             = google_compute_router.nat_router.name
   region                             = var.region
   nat_ip_allocate_option             = "MANUAL_ONLY"
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
