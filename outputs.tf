bf261cce556ca99a7f214ccdc5bff2e3208a79ad
Geoff Garbers <geoff@garbers.co.za>
2019-12-02 11:58:58 +0200

    Update outputs to include managed and unmanaged.

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,19 @@
-output "address_links" {
-  value       = [google_compute_address.nat_address.*.self_link]
+output "address_links_managed" {
+  value       = google_compute_address.nat_address.*.self_link
   description = "Self links of addresses managed by this module."
 }
 
-output "addresses" {
-  value       = [google_compute_address.nat_address.*.address]
+output "addresses_managed" {
+  value       = google_compute_address.nat_address.*.address
   description = "IPs of addresses managed by this module."
 }
 
+output "address_links" {
+  value = concat(google_compute_address.nat_address.*.self_link, data.google_compute_address.existing_addresses.*.self_link)
+  description = "Self links of all addresses in this module (both managed and unmanaged)."
+}
+
+output "addresses" {
+  value = concat(google_compute_address.nat_address.*.address, data.google_compute_address.existing_addresses.*.address)
+  description = "IPs of all addresses in this module (both managed and unmanaged)."
+}
