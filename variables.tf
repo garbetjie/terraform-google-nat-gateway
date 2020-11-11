1d377a443e7015455c6c9fcfbe786aaee6f707a1
Geoff Garbers <geoff@garbers.co.za>
2020-11-11 11:35:44 +0200

    Remov ability to reserve addresses in the module. Add logging config.

--- variables.tf
+++ variables.tf
@@ -1,84 +1,63 @@
-variable "name" {
-  type        = string
-  default     = ""
-  description = "The name of the Cloud NAT gateway. Defaults to `nat-$$${var.region}`."
+variable region {
+  type = string
+  description = "Region for which this Cloud NAT gateway is for."
 }
 
-variable "region" {
-  type        = string
-  description = "The region for which this Cloud NAT gateway is for"
+variable addresses {
+  type = list(string)
+  description = "Links or names of addresses to assign to the NAT gateway."
 }
 
-variable "address_links" {
-  type        = list(string)
-  default     = []
-  description = "The self link of addresses managed outside this module to use for the NAT gateway."
+variable name {
+  type = string
+  default = null
+  description = "Name of the Cloud NAT gateway. Defaults to `nat-$$${var.region}`."
 }
 
-variable "reserve_addresses" {
-  type        = string
-  default     = true
-  description = "Boolean value to indicate whether this module should reserve addresses for use with the NAT gateway."
-}
-
-variable "reserved_address_count" {
-  type        = string
-  default     = 1
-  description = "When reserving addresses, the number of addresses to reserve."
-}
-
-variable "reserved_address_name" {
-  type        = string
-  default     = "nat-%s-%02d"
-  description = "The name format to use when reserving addresses. Interpolated with the region and count.index + 1."
-}
-
-variable "reserved_address_description" {
-  type        = string
-  default     = "Cloud NAT gateway in %s."
-  description = "Description to use for managed addresses. Interpolated with the region."
-}
-
-variable "network" {
-  type        = string
-  default     = "default"
+variable network {
+  type = string
+  default = "default"
   description = "The network to which this Cloud NAT gateway applies."
 }
 
-variable "min_ports_per_vm" {
-  type        = string
-  default     = 128
+variable min_ports_per_vm {
+  type = number
+  default = 256
   description = "Minimum number of ports allocated to a VM."
 }
 
-variable "udp_idle_timeout_sec" {
-  type        = string
-  default     = 30
+variable udp_idle_timeout_sec {
+  type = number
+  default = 30
   description = "Timeout (in seconds) for UDP connections."
 }
 
-variable "tcp_established_idle_timeout_sec" {
-  type        = string
-  default     = 1200
+variable tcp_established_idle_timeout_sec {
+  type = number
+  default = 1200
   description = "Timeout (in seconds) for established TCP connections."
 }
 
-variable "tcp_transitory_idle_timeout_sec" {
-  type        = string
-  default     = 30
+variable tcp_transitory_idle_timeout_sec {
+  type = number
+  default = 30
   description = "Timeout (in seconds) for transitory TCP connections."
 }
 
-variable "icmp_idle_timeout_sec" {
-  type        = string
-  default     = 30
+variable icmp_idle_timeout_sec {
+  type = number
+  default = 30
   description = "Timeout (in seconds) for ICMP connections."
 }
 
-locals {
-  address_links = concat(
-    var.address_links,
-    google_compute_address.nat_address.*.self_link,
-  )
+variable logging_enabled {
+  type = bool
+  default = true
+  description = "Enable/disable logging on the NAT gateway."
 }
 
+variable logging_filter {
+  type = string
+  default = "ERRORS_ONLY"
+  description = "Filter to apply to NAT gateway logs."
+}
